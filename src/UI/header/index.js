d2c1a1cbcb747c8186c982e23dda5efeb5a49691
Rafael Flores Pereira <rafael.flores@codeby.com.br>
2021-09-08 09:05:10 -0300

    WIP: adding header structure

---
+++ src/UI/header/index.js
@@ -0,0 +1,16 @@
+import react from "react";
+import headerCard from "../../components/headerCard/index"
+
+const header = () => {
+    return (
+        <header>
+            <div className="header__background">
+                <h1 className="header__title">
+                    TITLE
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default header
---
+++ src/components/headerCard/index.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+const headerCard = () => {
+  return (
+    <div className="header-card">
+      <div className="header-card__top">
+        <div className="header-card__top--title">
+          Card title
+        </div>
+      </div>
+      <div className="header-card__content">
+        <div className="header-card__content--text">
+          card content text
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default headerCard
